e86678ab9dac76b9e4c608a8ec2fab23db2b86e8
Ahmed Saber <devahmedsaber@gmail.com>
2021-06-01 21:59:13 +0200

    Showing Statuses On The Timeline

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers;
 
 use Auth;
+use App\Models\Status;
 use Illuminate\Http\Request;
 
 class HomeController extends Controller
@@ -10,7 +11,11 @@ class HomeController extends Controller
     public function index(){
 
         if(Auth::check()){
-            return view('timeline.index');
+            $statuses = Status::where(function($query){
+                return $query->where('user_id', Auth::user()->id)->orWhereIn('user_id', Auth::user()->friends()->pluck('id'));
+            })->orderBy('created_at', 'desc')->paginate(2);
+            
+            return view('timeline.index')->with('statuses', $statuses);
         }
 
         return view('home');
--- resources/views/timeline/index.blade.php
+++ resources/views/timeline/index.blade.php
@@ -18,7 +18,34 @@
     </div>
     <div class="row">
         <div class="col-lg-5">
+            @if(!$statuses->count())
+                <p>There is Nothing In Your Timeline, Yet.</p>
+            @else
+                @foreach($statuses as $status)
+                    <div class="media">
+                        <a class="pull-left" href="{{ route('profile.index', ['username' => $status->user->username]) }}">
+                            <img class="media-object" src="{{ $status->user->getAvatarUrl() }}" alt="{{ $status->user->getNameOrUsername() }}">
+                        </a>
+                        <div class="media-body">
+                            <h4 class="media-heading"><a href="{{ route('profile.index', ['username' => $status->user->username]) }}">{{ $status->user->getNameOrUsername() }}</a></h4>
+                            <p>{{ $status->body }}</p>
+                            <ul class="list-inline">
+                                <li>{{ $status->created_at->diffForHumans() }}</li>
+                                <li><a href="#">Like</a></li>
+                                <li>10 Likes</li>
+                            </ul>
 
+                            <form role="form" action="#" method="post">
+                                <div class="form-group">
+                                    <textarea name="reply-1" class="form-control" placeholder="Reply to this status" rows="2"></textarea>
+                                </div>
+                                <input type="submit" value="Reply" class="btn btn-default btn-sm" name="" id="">
+                            </form>
+                        </div>
+                    </div>
+                @endforeach
+                {{ $statuses->render() }}
+            @endif
         </div>
     </div>
 @endsection
